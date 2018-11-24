e9f94404da2ee98589067bcfaa994773040c9ae0
luyunfei.louis <luyunfei.louis@bytedance.com>
2018-11-24 05:07:19 +0200

    move

---
+++ Opposite/Assets/Scripts/BallControl_1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl_1 : MonoBehaviour {
+
+
+    private static float unit = 0.1F;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + unit, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - unit, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - unit, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)){
+            transform.position = new Vector3(transform.position.x + unit, transform.position.y, transform.position.z);
+
+        }
+    }
+
+}
---
+++ Opposite/Assets/Scripts/BallControl_2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl_2 : MonoBehaviour {
+
+
+    private static float unit = 0.1F;
+
+    private Vector2 move;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + unit, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - unit, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - unit, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + unit, transform.position.y, transform.position.z);
+
+        }
+    }
+}
